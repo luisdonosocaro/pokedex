2112273385bf4b2bf78a695d15c08e308f2934bb
Luis Donoso <bahamutfft@gmail.com>
2024-08-12 20:12:09 -0400

    avances curso

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -59,8 +59,16 @@ export class PokemonService {
   }
 
   async remove(id: string) {
-    const pokemon = await this.findOne(id);
-    await pokemon.deleteOne();
+/*     const pokemon = await this.findOne(id);
+    await pokemon.deleteOne(); */
+    //const result = await this.pokemonModel.findByIdAndDelete(id);
+
+    const {deletedCount} = await this.pokemonModel.deleteOne({_id: id});
+
+    if(deletedCount === 0)
+      throw new BadRequestException(`Pokemon with id ${id} not found`);
+      
+    return;
   }
 
 
